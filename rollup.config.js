8d93fda1025c8bb031608d51dd9b5924505b21cf
Scott Moss <willscottmoss@gmail.com>
2019-04-23 14:47:27 -0700

    fix(build): fix build
    fix rollup build
    remove browser from p.json
    update gitignore

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,29 @@
 import typescript from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-
 import pkg from './package.json'
 import { terser } from 'rollup-plugin-terser'
+
+const plugins = [
+  resolve({
+    customResolveOptions: {
+      moduleDirectory: 'node_modules'
+    }
+  }),
+  commonjs(),
+  typescript({
+    typescript: require('typescript')
+  })
+]
+
 export default [
   {
     input: 'src/index.ts',
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({
-        typescript: require('typescript')
-      }),
-      terser()
-    ],
+    plugins: [...plugins, terser()],
     output: {
       file: pkg.browser,
       format: 'umd',
-      name: 'transformer',
+      name: 'tipeTransformer',
       esModule: false,
       globals: {
         'lodash.isstring': 'isString',
@@ -29,11 +34,7 @@ export default [
   },
   {
     input: 'src/index.ts',
-    plugins: [
-      typescript({
-        typescript: require('typescript')
-      })
-    ],
+    plugins,
     output: [
       {
         file: pkg.module,
@@ -44,9 +45,6 @@ export default [
         format: 'cjs'
       }
     ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.devDependancies || {})
-    ]
+    external: [...Object.keys(pkg.dependencies || {})]
   }
 ]
